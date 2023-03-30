1e0d7e98ccbbabff1959562a4b4853239f2f5e5c
Pabodha Wimalasuriya <pabodha.wimalasuriya@capgemini.com>
2023-03-30 16:55:30 +0800

    Generate OTP and Send Email Validate OTP

---
+++ Controllers/VerifyEmailOTPModel.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+// VerifyEmailOTPModel - The model used to get the Body from the Request.
+public class VerifyEmailOTPModel
+{
+    // StateId - This is the value we got as the return response from the `GenerateOTPEmail` method.
+    // This is a required field.
+    [Required(ErrorMessage = "Invalid Request - Please provide valid StateId.")]
+    public string? StateId { get; set; }
+
+    // OTP - This is the value we got as the OTP to the Email.
+    // This is a required field. And should have only 6 digits.
+    [Required(ErrorMessage = "Invalid Request - Please provide valid OTP.")]
+    [MaxLength(6)]
+    public string? OTP { get; set; }
+}
--- Program.cs
+++ Program.cs
@@ -1,11 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add Memory Caching
+builder.Services.AddMemoryCache();
 
 var app = builder.Build();
 
